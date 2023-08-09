fa3574553d9ed136b543139523aceaf25baa38c8
Bijay Sharma <58084675+bijaysharma8335@users.noreply.github.com>
2023-08-09 12:38:43 +0530

    Update Slider.jsx

--- src/Slider.jsx
+++ src/Slider.jsx
@@ -49,7 +49,7 @@ const Slider = () => {
     };
     return (
         <div className="container">
-            <div className="loadbar" style={{ width: `${loadingProgress}%` }}></div>{" "}
+            <div className="loadbar" style={{ width: `${loadingProgress}%` }}></div>
             <div id="slide" ref={slideRef}>
                 {data.map((item) => (
                     <div
@@ -59,8 +59,8 @@ const Slider = () => {
                     >
                         <div className="content">
                             <div className="name">{item.name}</div>
-                            <div className="desc">{item.desc}</div>
-                            <button>See More</button>
+                            <div className="des">{item.desc}</div>
+                            <button>See more</button>
                         </div>
                     </div>
                 ))}
@@ -78,30 +78,3 @@ const Slider = () => {
 };
 
 export default Slider;
-{/* <div className="container">
-<div className="loadbar" style={{ width: `${loadingProgress}%` }}></div>
-<div id="slide" ref={slideRef}>
-  {data.map((item) => (
-    <div
-      key={item.id}
-      className="item"
-      style={{ backgroundImage: `url(${item.imgUrl})` }}
-    >
-      <div className="content">
-        <div className="name">{item.name}</div>
-        <div className="des">{item.desc}</div>
-        <button>See more</button>
-      </div>
-    </div>
-  ))}
-</div>
-<div className="buttons">
-  <button id="prev" onClick={handleClickPrev}>
-    <FontAwesomeIcon icon={faAngleLeft} />
-  </button>
-  <button id="next" onClick={handleClickNext}>
-    <FontAwesomeIcon icon={faAngleRight} />
-  </button>
-</div>
-</div>
-); */}
